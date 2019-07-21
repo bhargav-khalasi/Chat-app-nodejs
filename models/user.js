f5254ecc935667bcb382ecad671782f2a7d071a1
develper <39995785+develper@users.noreply.github.com>
2019-07-21 19:22:29 +0530

    Add files via upload

---
+++ models/user.js
@@ -0,0 +1,52 @@
+const mongoose = require('mongoose');
+const Joi = require('joi');
+mongoose.set('useCreateIndex', true);
+mongoose.set('useFindAndModify', false);
+mongoose.pluralize(null);
+
+mongoose.connect('mongodb://localhost/ChatApp',{ useNewUrlParser: true })
+    .then(()=>console.log("Connected to the MongoDB"))
+    .catch((err)=>console.log(err.message));
+
+const User = mongoose.model('users',new mongoose.Schema({
+    name: {
+        type:String,
+        required: true,
+        minlength: 3,
+        maxlength: 30
+    },
+    email:{
+        type: String,
+        required: true,
+        unique: true,
+        minlength: 5,
+        maxlength: 255,
+    },
+    password:{
+        type: String,
+        required: true,
+        minlength: 5,
+        maxlength: 1024
+    },
+    isOnline:{
+        type: Boolean,
+        default: false
+    }
+}));
+const Chat = mongoose.model('chat_history',new mongoose.Schema({
+    from_id: String,
+    to_id: String,
+    message: String
+}));
+function validateUser(user)
+{
+    const schema = {
+        name: Joi.string().min(3).max(30).required(),
+        email: Joi.string().min(5).max(255).required().email(),
+        pass: Joi.string().min(5).max(1024).required()
+    }
+    return Joi.validate(user,schema);
+}
+
+exports.User = User;
+exports.Chat = Chat; 
---
+++ routes/users.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const router = express.Router();
+const bcrypt = require('bcrypt');
+const _ = require('lodash');
+const {User,Chat} = require('../models/user');
+const url = require('url');
+var userchat;
+
+ //Get
+ router.get('/', (req,res)=> res.render('login'));
+ router.get('/Signup',(req,res)=>res.render('register'));
+ router.get('/chat',(req,res)=>
+ {
+     if(req.query.user_id == null)return res.redirect("/");
+     res.render('chat',{user_id:req.query.user_id});
+ });
+ 
+
+ //Post
+router.post('/user/register',async (req,res)=>{
+    //const {error} = validate(req.body);
+    //if(error) return res.status(400).send(error.details[0].message);
+    let user = await User.findOne({email: req.body.email});
+    if(user) return res.status(400).send('Email Already registered.');
+    let userObject = new User({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.pass
+    });
+    const salt = await bcrypt.genSalt(10);
+    userObject.password = await bcrypt.hash(userObject.password,salt);
+    await userObject.save();
+    res.redirect('/');
+});
+
+router.post('/user/login',async (req,res)=>{
+    let user = await User.findOne({email: req.body.email});
+    if(!user) return res.status(400).send('This Email is not Registered');
+    const validPass = await bcrypt.compare(req.body.pass,user.password);
+    if(!validPass) return res.status(400).send('Invalid Email or Password');
+    User.updateOne({email: req.body.email}, {$set:{isOnline: true}},(err)=>{
+        if(err)return res.send(400).send("Something went wrong while updating");
+    });
+    res.redirect(url.format({
+        pathname: "/chat",
+        query:{
+            "user_id" : ""+user._id+"",
+        }
+    }));
+});
+
+module.exports = router;
